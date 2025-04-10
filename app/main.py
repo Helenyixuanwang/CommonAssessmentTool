1beeeb3dc911f2939e8d5325bf195dba8fc760f6
Helen Wang <wangyixuan@msn.com>
2025-04-10 10:30:29 -0700

    new modification to test CI pipeline

--- app/main.py
+++ app/main.py
@@ -3,7 +3,8 @@ Main application module for the Common Assessment Tool.
 This module initializes the FastAPI application and includes all routers.
 Handles database initialization and CORS middleware configuration.
 """
-
+# Add an import that isn't used
+import datetime
 from fastapi import FastAPI
 from app import models
 from app.database import engine
